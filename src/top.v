28823d15b6818b78a7b929f9dec33b8f48e77689
lzzsG <lizizhi122@gmail.com>
2024-11-10 10:29:56 +0800

    update makefile

---
+++ src/top.v
@@ -0,0 +1,13 @@
+module top (
+    input  [3:0] a,
+    input  [3:0] b,
+    input  [1:0] opcode,
+    output [3:0] result
+);
+  alu u_alu (
+      .a(a),
+      .b(b),
+      .opcode(opcode),
+      .result(result)
+  );
+endmodule
